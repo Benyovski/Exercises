dce85de1430b04dcb08bb8712297f615f12514ba
Konrad Lacek <konrad.lacek@outlook.com>
2024-12-29 22:55:58 +0100

    Two new simple exercises.

---
+++ HCRNKCompareTriplets/HCRNKCompareTriplets/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+
+namespace HCRNKCompareTriplets
+{
+    internal class Program
+    {
+        public static List<int> CompareTriplets(List<int> a, List<int> b)
+        {
+            int aliceScore = 0;
+            int bobScore = 0;
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (a[i] > b[i])
+                {
+                    aliceScore++;
+                }
+                else if (a[i] < b[i])
+                {
+                    bobScore++;
+                }
+            }
+
+            return new List<int> { aliceScore, bobScore };
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ HCRNKSumOfArray/HCRNKSumOfArray/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+
+namespace HCRNKSumOfArray
+{
+    internal class Program
+    {
+        public static int SumOfArray(List<int> arr)
+        {
+            int sum = 0;
+            foreach (int i in arr)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
