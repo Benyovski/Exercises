3c36f1e741bbf68de3f588fb2547ddbf014210d4
Konrad Lacek <konrad.lacek@outlook.com>
2024-12-29 22:27:23 +0100

    Removal of redunant code.

--- LCAddTwoNumbers/LCAddTwoNumbers/Program.cs
+++ LCAddTwoNumbers/LCAddTwoNumbers/Program.cs
@@ -49,26 +49,7 @@
         }
         static void Main(string[] args)
         {
-            ListNode l3 = new(3, null);
-            ListNode l2 = new(4, l3);
-            ListNode l1 = new(2, l2);
-            ListNode[] ll1 = [l1, l2, l3];
-
-            ListNode l6 = new(4, null);
-            ListNode l5 = new(6, l6);
-            ListNode l4 = new(5, l5);
-            ListNode[] ll2 = [l4, l5, l6];
-            ListNode[] rl = new ListNode[3];
-
-            ListNode result = new();
-            for (int i = 0; i < ll1.Length; i++)
-            {
-                rl[i] = AddTwoNumbers(ll1[i], ll2[i]);
-            }
-            for(int i = 0;i < rl.Length; i++)
-            {
-                Console.WriteLine(rl[i].val);
-            }
+            Console.WriteLine("Hello World!");
         }
     }
 }
