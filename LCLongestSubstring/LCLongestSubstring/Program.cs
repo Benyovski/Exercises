10d05f6d53e541ddb6af1b15c3d4bc5120264afa
Konrad Lacek <konrad.lacek@outlook.com>
2024-12-30 13:50:00 +0100

    Adding a LeetCode exercise for finding the longest substring of a string that does not contain repeated characters.

---
+++ LCLongestSubstring/LCLongestSubstring/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace LCLongestSubstring
+{
+    internal class Program
+    {
+        public static int LongestSubstring(string s)
+        {
+            int len = s.Length;
+            int tempLength = 0;
+            List<int> listLength = new List<int>();
+            if (len == 0)
+            {
+                return 0;
+            }
+            for (int i = 0; i < len; i++)
+            {
+                for (int j = i; j < len; j++)
+                {
+                    if (!s.Substring(i, j - i).Contains(s[j]))
+                    {
+                        tempLength++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                listLength.Add(tempLength);
+                tempLength = 0;
+            }
+
+            return listLength.Max();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LongestSubstring("abcabcbb").ToString());
+        }
+    }
+}
