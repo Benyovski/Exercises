7d460b87431a4ffac958f90e6015ed73c64ddcff
Konrad Lacek <konrad.lacek@outlook.com>
2024-12-31 18:27:58 +0100

    A simple HackerRank exercise.

---
+++ HCRNKCandles/HCRNKCandles/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HCRNKCandles
+{
+    public class Program
+    {
+        public static int BirthdayCakeCandles(List<int> candles)
+        {
+            int sum = default;
+            int maximumCandleHeight = candles.Max();
+            foreach (int candle in candles)
+            {
+                if(candle == maximumCandleHeight)
+                {
+                    sum++;
+                }
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ HCRNKCandles/HCRNKSCandlesTests/Test1.cs
@@ -0,0 +1,20 @@
+﻿namespace HCRNKSCandlesTests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            List<int> candles = new List<int> { 4, 4, 4, 4, 4 };
+            Assert.AreEqual(5, HCRNKCandles.Program.BirthdayCakeCandles(candles));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            List<int> candles = new List<int> { 4, 4, 1, 3 };
+            Assert.AreEqual(2, HCRNKCandles.Program.BirthdayCakeCandles(candles));
+        }
+    }
+}
