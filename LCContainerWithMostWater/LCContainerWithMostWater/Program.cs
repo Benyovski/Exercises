c48485675511604d76842c3b5515af1b275af330
Konrad Lacek <konrad.lacek@outlook.com>
2025-01-03 16:29:06 +0100

    LeetCode ContainerWithMostWater (Two Pointer Approach) + Beta O(n^2) method (Brute Force Approach) + Appropriate tests

---
+++ LCContainerWithMostWater/LCContainerWithMostWater/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace LCContainerWithMostWater
+{
+    public class Program
+    {
+        /// <summary>
+        /// A function to find the maximum area of water that can be contained in a container; Approach: Brute Force. Unoptimal, does not pass all test cases.
+        /// </summary>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public int MaxAreaBeta(int[] height)
+        {
+            int maxArea = 0;
+            for (int i = 0; i < height.Length; i++)
+            {
+                for (int j = i + 1; j < height.Length; j++)
+                {
+                    int area = Math.Min(height[i], height[j]) * (j - i);
+                    maxArea = Math.Max(maxArea, area);
+                }
+            }
+            return maxArea;
+        }
+        /// <summary>
+        /// A function to find the maximum area of water that can be contained in a container; Approach: Two Pointer. Optimal, passes all test cases.
+        /// </summary>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public int MaxArea(int[] height)
+        {
+            int maxArea = 0;
+            int left = 0;
+            int right = height.Length - 1;
+            while (left < right)
+            {
+                int area = Math.Min(height[left], height[right]) * (right - left);
+                maxArea = Math.Max(maxArea, area);
+                if (height[left] < height[right])
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+            return maxArea;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ LCContainerWithMostWater/LCContainerWithMostWaterTests/Test1.cs
@@ -0,0 +1,18 @@
+﻿namespace LCContainerWithMostWaterTests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(49, new LCContainerWithMostWater.Program().MaxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(1, new LCContainerWithMostWater.Program().MaxArea([1, 1]));
+        }
+    }
+}
