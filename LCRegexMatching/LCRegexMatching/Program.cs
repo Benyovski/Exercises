26f25ce80ba62f4feaec3a72d742c782a68473e5
Konrad Lacek <konrad.lacek@outlook.com>
2025-01-03 16:04:43 +0100

    An initial attempt at LC Regex Matching, a beta function that will be modified

---
+++ LCRegexMatching/LCRegexMatching/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LCRegexMatching
+{
+    public class Program
+    {
+        /// <summary>
+        /// A stub of a method that should be tested.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public bool IsMatchBeta(string s, string p)
+        {
+            if (string.IsNullOrEmpty(s) || string.IsNullOrEmpty(p))
+            {
+                return false;
+            }
+            if(p.Length == 1)
+            {
+                return s.Length == 1 && (s[0] == p[0] || p[0] == '.');
+            }
+            return Regex.IsMatch(s, p);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ LCRegexMatching/LCRegexMatchingTests/Test1.cs
@@ -0,0 +1,24 @@
+﻿namespace LCRegexMatchingTests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsFalse(new LCRegexMatching.Program().IsMatchBeta("aa", "a"));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(new LCRegexMatching.Program().IsMatchBeta("aa", "a*"));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.IsTrue(new LCRegexMatching.Program().IsMatchBeta("ab", ".*"));
+        }
+    }
+}
