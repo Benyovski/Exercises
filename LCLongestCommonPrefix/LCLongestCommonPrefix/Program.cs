0e5303cce9f6c5a0e56f8d3fbbc232499ee53568
Konrad Lacek <konrad.lacek@outlook.com>
2025-01-03 19:42:40 +0100

    LeetCode LongestCommonPrefix + Appropriate Tests

---
+++ LCLongestCommonPrefix/LCLongestCommonPrefix/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace LCLongestCommonPrefix
+{
+    public class Program
+    {
+
+        /// <summary>
+        /// A function to find the longest common prefix string amongst an array of strings. If there is no common prefix, it returns an empty string "". 
+        /// </summary>
+        /// <param name="strs"></param>
+        /// <returns></returns>
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0)
+            {
+                return "";
+            }
+            if (strs.Length == 1)
+            {
+                return strs[0];
+            }
+            int minLength = int.MaxValue;
+            foreach (string str in strs)
+            {
+                if (str.Length < minLength)
+                {
+                    minLength = str.Length;
+                }
+            }
+            System.Text.StringBuilder longestCommonPrefix = new();
+            for (int i = 0; i < minLength; i++)
+            {
+                char common = strs[0][i];
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    if (strs[j][i] != common)
+                    {
+                        return longestCommonPrefix.ToString();
+                    }
+                }
+                longestCommonPrefix.Append(common);
+            }
+            return longestCommonPrefix.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ LCLongestCommonPrefix/LCLongestCommonPrefixTests/Test1.cs
@@ -0,0 +1,36 @@
+﻿namespace LCLongestCommonPrefixTests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("fl", new LCLongestCommonPrefix.Program().LongestCommonPrefix(["flower", "flow", "flight"]));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual("", new LCLongestCommonPrefix.Program().LongestCommonPrefix(["dog", "racecar", "car"]));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual("a", new LCLongestCommonPrefix.Program().LongestCommonPrefix(["a"]));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual("", new LCLongestCommonPrefix.Program().LongestCommonPrefix([]));
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.AreEqual("alf", new LCLongestCommonPrefix.Program().LongestCommonPrefix(["alfesliugfhbsaoil", "alfhpjppl"]));
+        }
+    }
+}
