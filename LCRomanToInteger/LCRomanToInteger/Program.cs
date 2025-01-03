b801c3e887ba417e4b02985e6c08e512b21a4211
Konrad Lacek <konrad.lacek@outlook.com>
2025-01-03 19:08:16 +0100

    LeetCode RomanToInteger, not optimal but working + Appropriate Tests

---
+++ LCRomanToInteger/LCRomanToInteger/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace LCRomanToInteger
+{
+    public class Program
+    {
+        /// <summary>
+        /// Given a roman numeral string, convert it to an integer.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public int RomanToInt(string s)
+        {
+            if (s == null || s.Length == 0)
+            {
+                return 0;
+            }
+            int[] values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+            string[] symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+            int result = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 0; j < symbols.Length; j++)
+                {
+                    if (s.Substring(i).StartsWith(symbols[j]))
+                    {
+                        result += values[j];
+                        i += symbols[j].Length - 1;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ LCRomanToInteger/LCRomanToIntegerTests/Test1.cs
@@ -0,0 +1,44 @@
+﻿using LCRomanToInteger;
+
+namespace LCRomanToIntegerTests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(3, new Program().RomanToInt("III"));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(4, new Program().RomanToInt("IV"));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(9, new Program().RomanToInt("IX"));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(58, new Program().RomanToInt("LVIII"));
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.AreEqual(1994, new Program().RomanToInt("MCMXCIV"));
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Assert.AreEqual(621, new Program().RomanToInt("DCXXI"));
+        }
+    }
+}
