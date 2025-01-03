71f126330cadb500f32cf98167e480524f7e44a5
Konrad Lacek <konrad.lacek@outlook.com>
2025-01-03 18:37:30 +0100

    LeetCode IntegerToRoman + Appropriate Tests

---
+++ LCIntegerToRoman/LCIntegerToRoman/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace LCIntegerToRoman
+{
+    public class Program
+    {
+        /// <summary>
+        /// Given an integer, convert it to a roman numeral. Input is guaranteed to be within the range from 1 to 3999. 
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public string IntToRoman(int num)
+        {
+            if (num < 1 || num > 3999)
+            {
+                return string.Empty;
+            }
+            int[] values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+            string[] symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+            System.Text.StringBuilder romanNumber = new();
+            for (int i = 0; i < values.Length && num > 0; i++)
+            {
+                if (num >= values[i])
+                {
+                    romanNumber.Append(symbols[i]);
+                    num -= values[i];
+                    i--;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return romanNumber.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ LCIntegerToRoman/LCIntegerToRomanTests/Test1.cs
@@ -0,0 +1,67 @@
+﻿namespace LCIntegerToRomanTests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("III", new LCIntegerToRoman.Program().IntToRoman(3));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual("IV", new LCIntegerToRoman.Program().IntToRoman(4));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual("IX", new LCIntegerToRoman.Program().IntToRoman(9));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual("LVIII", new LCIntegerToRoman.Program().IntToRoman(58));
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.AreEqual("MCMXCIV", new LCIntegerToRoman.Program().IntToRoman(1994));
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Assert.AreEqual("MMMCMXCIX", new LCIntegerToRoman.Program().IntToRoman(3999));
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Assert.AreEqual(string.Empty, new LCIntegerToRoman.Program().IntToRoman(0));
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            Assert.AreEqual(string.Empty, new LCIntegerToRoman.Program().IntToRoman(4000));
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            Assert.AreEqual(string.Empty, new LCIntegerToRoman.Program().IntToRoman(-1));
+        }
+
+        [TestMethod]
+        public void TestMethod10()
+        {
+            Assert.AreEqual("MMMDCCXLIX", new LCIntegerToRoman.Program().IntToRoman(3749));
+        }
+
+    }
+}
