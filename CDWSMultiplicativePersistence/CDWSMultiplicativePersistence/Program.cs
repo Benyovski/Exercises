ca92174067a13e8b0e6841868b9922869c1790d9
Konrad Lacek <konrad.lacek@outlook.com>
2025-01-08 20:24:12 +0100

    CodeWars Multiplicative Persistence + Appropriate Tests

---
+++ CDWSMultiplicativePersistence/CDWSMultiplicativePersistence/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace CDWSMultiplicativePersistence
+{
+    public class Program
+    {
+        /// <summary>
+        /// A method that takes a positive parameter n and returns its multiplicative persistence, which is the number of times you must multiply the digits in n until you reach a single digit.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int Persistence(long n)
+        {
+            if (n < 10)
+            {
+                return 0;
+            }
+            int product = 1;
+            string[] digits = n.ToString().ToCharArray().Select(c => c.ToString()).ToArray();
+            for (int i = 0; i < digits.Length; i++)
+            {
+                product *= int.Parse(digits[i]);
+            }
+            if(product < 10)
+            {
+                return 1;
+            }
+            else
+            {
+                return 1 + Persistence(product);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Persistence(999).ToString());
+        }
+    }
+}
---
+++ CDWSMultiplicativePersistence/CDWSMultiplicativePersistenceTests/MultiplicativePersistenceTests.cs
@@ -0,0 +1,30 @@
+﻿namespace CDWSMultiplicativePersistenceTests
+{
+    [TestClass]
+    public sealed class MultiplicativePersistenceTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(0, CDWSMultiplicativePersistence.Program.Persistence(4));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(3, CDWSMultiplicativePersistence.Program.Persistence(39));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(4, CDWSMultiplicativePersistence.Program.Persistence(999));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(2, CDWSMultiplicativePersistence.Program.Persistence(25));
+        }
+    }
+}
