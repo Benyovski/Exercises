a89503380884dec7a65083ceea69a84b7bf36413
Konrad Lacek <konrad.lacek@outlook.com>
2025-01-09 14:51:18 +0100

    A yet another CodeWars exercise.

---
+++ CDWSArrayDifference/CDWSArrayDifference/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CDWSArrayDifference
+{
+    public class Program
+    {
+        public static int[] ArrayDiff(int[] a, int[] b)
+        {
+            List<int> aList = a.ToList();
+            List<int> bList = b.ToList();
+            List<int> result = new();
+            foreach (int i in aList)
+            {
+                if (!bList.Contains(i))
+                {
+                    result.Add(i);
+                }
+            }
+            return result.ToArray();
+        } 
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
