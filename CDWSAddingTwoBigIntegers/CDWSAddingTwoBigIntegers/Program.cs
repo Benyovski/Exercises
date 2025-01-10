e295a6eeb15d7daeb7233c8d3b3dfaa938546a46
Konrad Lacek <konrad.lacek@outlook.com>
2025-01-10 22:07:58 +0100

    CodeWars Adding Two Big Integers

---
+++ CDWSAddingTwoBigIntegers/CDWSAddingTwoBigIntegers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace CDWSAddingTwoBigIntegers
+{
+    internal class Program
+    {
+        public static string Add(string a, string b)
+        {
+            BigInteger aInt = BigInteger.Parse(a);
+            BigInteger bInt = BigInteger.Parse(b);
+            BigInteger resultInt = aInt + bInt;
+            return resultInt.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
